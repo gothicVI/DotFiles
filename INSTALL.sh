d8e51ba55f441a1ea0005ec0e4919025a75b8a54
gothicVI <33968289+gothicVI@users.noreply.github.com>
2019-03-04 12:38:29 +0100

    Add files via upload

---
+++ INSTALL.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $HOME
+echo " === Downloading DotFiles from https://github.com/gothicVI/DotFiles === "
+echo " === This is done interactively === "
+echo " === Make sure to have a backup! === "
+echo ""
+echo "Downloading .bash_aliases, .bash_logout, .bashrc, .checkifsourced, .cmdpromt, .profile"
+echo "Hit ENTER to continue or Ctrl+C to abort"
+echo ""
+read -s
+wget https://raw.githubusercontent.com/gothicVI/DotFiles/master/.bash_aliases https://raw.githubusercontent.com/gothicVI/DotFiles/master/.bash_logout https://raw.githubusercontent.com/gothicVI/DotFiles/master/.bashrc https://raw.githubusercontent.com/gothicVI/DotFiles/master/.checkifsourced https://raw.githubusercontent.com/gothicVI/DotFiles/master/.cmdpromt https://raw.githubusercontent.com/gothicVI/DotFiles/master/.profile
+echo ""
+while true; do
+        read -p "Is this a laptop? Type Y/y or N/n and hit ENTER: " laptop
+        case $laptop in
+                [Yy]* ) echo ""; wget https://raw.githubusercontent.com/gothicVI/DotFiles/master/.IsLaptop; break;;
+                [Nn]* ) echo ""; break;;
+        esac
+done
+echo ""
+while true; do
+        read -p "Do you compile Android and want to use ccache and limit the RAM consumption for jack? \n Type 5 for a 5GM limit or 20 for a 20GB limit or N/n and hit ENTER: " android
+        case $android in
+                [5]* ) echo ""; wget https://raw.githubusercontent.com/gothicVI/DotFiles/master/.android5G; break;;
+                [20]* ) echo ""; wget https://raw.githubusercontent.com/gothicVI/DotFiles/master/.android20G; break;;
+                [Nn]* ) echo ""; break;;
+        esac
+done
+echo ""
+echo " === We're done! Deleting this installation script now: === "
+rm -v -- "$0"
+exit
