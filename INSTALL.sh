0e911e11e920aef9f17de329097671459e86c4a5
Sebastian Steinbeißer <sebastian.steinbeisser@googlemail.com>
2020-03-24 11:38:54 +0100

    Add more options to limit RAM consumption...
    ...when building android. Also set CCACHE compression to true.

--- INSTALL.sh
+++ INSTALL.sh
@@ -34,21 +34,34 @@ done
 echo ""
 while true; do
         echo "Do you compile Android and want to use ccache and limit the RAM consumption for jack?"
-        read -p " Type 5 for a 5GM limit or 20 for a 20GB limit or N/n and hit ENTER: " android
+        read -p " Type 4 for a 4GM limit, or 8 for a 8GB limit, or 16 for a 16GB limit or N/n and hit ENTER: " android
         case $android in
-                [5]* )  echo "";
+                [4]* )  echo "";
                     if [ -f .Android ]; then
                     rm .Android
                     fi;
                     echo 'export USE_CCACHE=1' >> .Android;
-                    echo 'export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx5G"' >> .Android;
+                    echo 'export CCACHE_COMPRESS=1' >> .Android;
+                    echo 'export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx4G"' >> .Android;
+                    echo 'export JAVA_TOOL_OPTIONS=-Xmx4G' >> .Android
                     break;;
-                [20]* ) echo "";
+                [8]* ) echo "";
                     if [ -f .Android ]; then
                     rm .Android
                     fi;
                     echo 'export USE_CCACHE=1' >> .Android;
-                    echo 'export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx20G"' >> .Android;
+                    echo 'export CCACHE_COMPRESS=1' >> .Android;
+                    echo 'export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx8G"' >> .Android;
+                    echo 'export JAVA_TOOL_OPTIONS=-Xmx8G' >> .Android
+                    break;;
+                [16]* ) echo "";
+                    if [ -f .Android ]; then
+                    rm .Android
+                    fi;
+                    echo 'export USE_CCACHE=1' >> .Android;
+                    echo 'export CCACHE_COMPRESS=1' >> .Android;
+                    echo 'export ANDROID_JACK_VM_ARGS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx16G"' >> .Android;
+                    echo 'export JAVA_TOOL_OPTIONS=-Xmx16G' >> .Android
                     break;;
                 [Nn]* ) echo ""; break;;
         esac
